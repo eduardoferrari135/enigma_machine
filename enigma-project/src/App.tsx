9db3f5716276d455415498bfd15549d6f04b4337
Ficalabro <141239313+Ficalabro@users.noreply.github.com>
2025-11-28 09:30:30 -0300

    add frontend da máquina enigma

---
+++ enigma-project/src/App.tsx
@@ -0,0 +1,11 @@
+import EnigmaSimulator from './enigma/EnigmaSimulator';
+
+function App() {
+  return (
+    <div style={{ width: '100%', minHeight: '100vh', margin: 0, padding: 0 }}>
+      <EnigmaSimulator />
+    </div>
+  );
+}
+
+export default App;
